1e5e7d36b82c508787f9475a7cb167a3ce243154
jdnichollsc <jdnichollsc@hotmail.com>
2024-12-04 13:56:48 -0500

    code review

--- apps/web/src/app/pages/home.page.ts
+++ apps/web/src/app/pages/home.page.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { TodoItemComponent } from "@todo-app/ui";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TodoItemComponent],
   templateUrl: './home.page.html'
 })
 export default class HomePage {
